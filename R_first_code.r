fa290746378e9c17b07a5a2e6857956946c203bc
Victor Hugo Guerrero <121627328+VictorHugoGuerrero@users.noreply.github.com>
2022-12-30 19:02:19 -0500

    Create R_first_code.r

---
+++ R_first_code.r
@@ -0,0 +1,13 @@
+# This is my first code in github!
+
+# Here are the input data
+#costanza data on streams
+water <- c(100, 200, 300, 400, 500)
+water
+
+# Marta data on fishes genomes
+fishes <- c(10, 50, 60, 100, 200)
+fishes
+
+# plot(x,y) <- x = independent, y = dependent
+plot(water, fishes)
